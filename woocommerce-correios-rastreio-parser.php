1e010c825e24df7f3ffae2ab1d3b85d2d66c6e0d
System Administrator <root@AsafeMac.local>
2018-05-13 00:01:33 -0300

    arquivo do plugin

---
+++ woocommerce-correios-rastreio-parser.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: WooCommerce Correios Rastreio Parser
+ * Description: Atualize automaticamente o andamento do pedido com o rastreio dos Correios
+ * Plugin URI: http://asaferamos.com
+ * Author: Asafe Ramos
+ * Author URI: http://asaferamos.com
+ * Version: 1.0
+ * Requires at least: 4.2
+ * License: GPL3
+*/
